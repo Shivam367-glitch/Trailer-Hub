d6dc971ae2a4f9038344843c61722d91e143267a
Shivam <txt2shivam@gmail.com>
2025-05-22 16:23:56 +0530

    Congif for Video Player

--- src/components/VideoBackground.jsx
+++ src/components/VideoBackground.jsx
@@ -1,7 +1,7 @@
 import { useSelector } from "react-redux";
 import useMovieTrailer from "../hooks/useMovieTrailer";
 import ReactPlayer from "react-player/lazy";
-
+import YOUTUBE_BASE_LINK from "../utils/Constants";
 const VideoBackground = ({ id }) => {
   useMovieTrailer(id);
   const nowPlaying = useSelector((store) => store?.nowPlaying);
@@ -10,8 +10,9 @@ const VideoBackground = ({ id }) => {
 
   return (
     <ReactPlayer
-      url={`https://www.youtube.com/watch?v=${videoId}`}
-      playing={true}
+      url={`${YOUTUBE_BASE_LINK}${videoId}`}
+      playing={true} 
+      loop={true}
       muted={true}
       className="position-absolute m-0 p-0 top-0"
       width="100%"
--- src/components/VideoTrailer.jsx
+++ src/components/VideoTrailer.jsx
@@ -1,40 +1,47 @@
-import Modal from 'react-bootstrap/Modal';
-import useMovieTrailer from '../hooks/useMovieTrailer';
-import { useSelector } from 'react-redux';
-import ReactPlayer from 'react-player/lazy';
-
-const VideoTrailer = (props) => { 
-    useMovieTrailer(props?.id); 
-    const videoId=useSelector((store)=>store?.nowPlaying?.videoId); 
-
+import Modal from "react-bootstrap/Modal";
+import useMovieTrailer from "../hooks/useMovieTrailer";
+import { useSelector } from "react-redux";
+import ReactPlayer from "react-player/lazy";
+import YOUTUBE_BASE_LINK from "../utils/Constants";
+const VideoTrailer = (props) => {
+  useMovieTrailer(props?.id);
+  const videoId = useSelector((store) => store?.nowPlaying?.videoId);
 
   return (
     <Modal
-    {...props}
-    size="lg"
-    aria-labelledby="contained-modal-title-vcenter"
-    fullscreen={true}
-    className='bg-dark'
-  >
-    <Modal.Header closeButton className='bg-dark text-white'>
+      {...props}
+      size="lg"
+      aria-labelledby="contained-modal-title-vcenter"
+      fullscreen={true}
+      className="bg-dark"
+    >
+      <Modal.Header closeButton className="bg-dark text-white">
         <Modal.Title>Play Trailer</Modal.Title>
-    </Modal.Header>
-    <Modal.Body className='bg-dark m-0 p-0'>
-   {
-    videoId?<ReactPlayer url={`https://www.youtube.com/watch?v=${videoId}` } controls={true} playing={true}  muted={true}   width='100%'
-              height='100%' />:<p className='text-white fs-5'>Trailer Not Available !</p>
-   }
-    </Modal.Body>
-  </Modal>
-  )
-}
+      </Modal.Header>
+      <Modal.Body className="bg-dark m-0 p-0">
+        {videoId ? (
+          <ReactPlayer
+            url={`${YOUTUBE_BASE_LINK}${videoId}`}
+            controls={true}
+            playing={true}
+            muted={true}
+            width="100%"
+            height="100%"
+          />
+        ) : (
+          <p className="text-white fs-5">Trailer Not Available !</p>
+        )}
+      </Modal.Body>
+    </Modal>
+  );
+};
 
-export default VideoTrailer
+export default VideoTrailer;
 
-//  <iframe 
+//  <iframe
 //         src={`https://www.youtube.com/embed/${videoId}?autoplay=0&loop=1&mute=1&controls=1`}
 //         className='w-100 h-100'
-//        title={"video player"} 
+//        title={"video player"}
 //       allow={"accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" }
 //       allowfullscreen
 //   ></iframe>
