1a46b23d975cb1d4f7c431b9ba4e40edb7c3146c
Shivam <txt2shivam@gmail.com>
2025-06-15 22:31:07 +0530

    Changes In Header

--- src/components/Dropdown.jsx
+++ src/components/Dropdown.jsx
@@ -1,75 +1,25 @@
-import   { useState } from 'react';
+import { NavDropdown } from 'react-bootstrap';
+import { NavLink } from 'react-router-dom';
 
-import Select from 'react-select';
-
-const Checkbox = ({ children, ...props }) => (
-  <label style={{ marginRight: '1em' }}>
-    <input type="checkbox" {...props} />
-    {children}
-  </label>
-);
-
-const Dropdown=() => {
-  const [isClearable, setIsClearable] = useState(true);
-  const [isSearchable, setIsSearchable] = useState(true);
-  const [isDisabled, setIsDisabled] = useState(false);
-  const [isLoading, setIsLoading] = useState(false);
-  const [isRtl, setIsRtl] = useState(false);
-
+const Dropdown = ({ title, links }) => {
   return (
-    <>
-      <Select
-        className="basic-single"
-        classNamePrefix="select"
-        defaultValue={colourOptions[0]}
-        isDisabled={isDisabled}
-        isLoading={isLoading}
-        isClearable={isClearable}
-        isRtl={isRtl}
-        isSearchable={isSearchable}
-        name="color"
-        options={colourOptions}
-      />
-
-      <div
-        style={{
-          color: 'hsl(0, 0%, 40%)',
-          display: 'inline-block',
-          fontSize: 12,
-          fontStyle: 'italic',
-          marginTop: '1em',
-        }}
-      >
-        <Checkbox
-          checked={isClearable}
-          onChange={() => setIsClearable((state) => !state)}
-        >
-          Clearable
-        </Checkbox>
-        <Checkbox
-          checked={isSearchable}
-          onChange={() => setIsSearchable((state) => !state)}
-        >
-          Searchable
-        </Checkbox>
-        <Checkbox
-          checked={isDisabled}
-          onChange={() => setIsDisabled((state) => !state)}
-        >
-          Disabled
-        </Checkbox>
-        <Checkbox
-          checked={isLoading}
-          onChange={() => setIsLoading((state) => !state)}
-        >
-          Loading
-        </Checkbox>
-        <Checkbox checked={isRtl} onChange={() => setIsRtl((state) => !state)}>
-          RTL
-        </Checkbox>
-      </div>
-    </>
+    <NavDropdown title={title} id={`offcanvasNavbarDropdown-expand`}>
+      {
+        links.map((link, ind) => (
+          <NavDropdown.Item 
+            as={NavLink} 
+            to={link.url} 
+            key={link.url + ind}
+          >
+            {link.title}
+          </NavDropdown.Item>
+        ))
+      }
+    </NavDropdown>
   );
-};
+}
 
 export default Dropdown;
+
+
+{/* <img src="person.png" alt="User Icon" className='img-fluid ' style={{ width: '30px', height: '30px' }} /> */}
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,4 @@
 import { useState } from "react";
-import Button from "react-bootstrap/Button";
 import Container from "react-bootstrap/Container";
 import { FaArrowRightLong } from "react-icons/fa6";
 import Nav from "react-bootstrap/Nav";
@@ -9,16 +8,26 @@ import Offcanvas from "react-bootstrap/Offcanvas";
 import { NavLink } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { toggleGpt } from "../utils/gptSlice";
+import Dropdown from "./Dropdown";
 function OffcanvasExample() {
   const [expanded, setExpanded] = useState(false);
   const dispatch=useDispatch(); 
   const showGpt=useSelector((store)=>store.gpt.showGptSearch);
   const user=useSelector((store)=>store.user);
   const handleCloseMenu = () => setExpanded(false);
+
+  const links=[{
+                  url:"/profile",
+                  title:"Profile"
+                 },
+                 {
+                   url:"/logout",
+                  title:"Log Out"
+                 }]
   const expand=false;
   return (
     <>
-      <Navbar expand={false} className="bg-transparent z-3 d-flex flex-row" >
+      <Navbar expand={"md"} className="bg-secondary bg-opacity-50 z-3 d-flex flex-row">
         <Container fluid={true}>
         <Navbar.Brand  as={NavLink} to={user?"/browser":"/"} > 
           <img src="logo.png" alt="Trailer Hub Logo" className='img-fluid w-18'/>
@@ -30,35 +39,39 @@ function OffcanvasExample() {
             <Navbar.Offcanvas
               id={`offcanvasNavbar-expand-${expand}`}
               aria-labelledby={`offcanvasNavbarLabel-expand-${expand}`}
-              placement="end"
+             placement="end"
+            
             >
-              <Offcanvas.Header closeButton>
+              <Offcanvas.Header closeButton  >
                 <Offcanvas.Title id={`offcanvasNavbarLabel-expand-${expand}`}>
                   <img src="logo.png" alt="Trailer Hub Logo" className='img-fluid w-18'/>
                 </Offcanvas.Title>
               </Offcanvas.Header>
-              <Offcanvas.Body>
-                <Nav className="justify-content-end flex-grow-1 pe-3 fs-5 fw-bolder">
+              {
+                user &&
+              <Offcanvas.Body className="d-flex flex-row justify-content-center" >
+                <Nav className="justify-content-start  flex-grow-1 fw-bolder">
                 <Nav.Link as={NavLink} to={'/browser'} >Welcome</Nav.Link>
                 <Nav.Link as={NavLink} to={'/people'}>People</Nav.Link>
                  {/* <Nav.Link as={NavLink} to={'/watch-history'}>History</Nav.Link>  */}
                  <Nav.Link  onClick={()=>{
                   dispatch(toggleGpt(true));
                  }}>AI Recommendation</Nav.Link>
-                {/* <Nav.Link as={NavLink} to={'/logout'}>Log Out</Nav.Link> */}
-              
-                  <NavDropdown
-                    title={user?.displayName?user.displayName:"User"}
-                    id={`offcanvasNavbarDropdown-expand-${expand}`}
+                  {/* <NavDropdown
+                    title={<img src="person.png" alt="User Icon" className='img-fluid ' style={{ width: '30px', height: '30px' }} />}
+                    id={`offcanvasNavbarDropdown-${expand}`}
                   >
                     <NavDropdown.Item>
                       <Nav.Link as={NavLink} to={'/profile'}>Profile</Nav.Link>
                     </NavDropdown.Item>
                     <NavDropdown.Item> <Nav.Link as={NavLink} to={'/logout'}>Log Out</Nav.Link></NavDropdown.Item>
-                  </NavDropdown>
+                  </NavDropdown> */} 
+                 <Dropdown title={<img src="person.png" alt="User Icon" className='img-fluid ' style={{ width: '30px', height: '30px' }} />} links={links}/>
+
                 </Nav>
                  
               </Offcanvas.Body>
+              }
             </Navbar.Offcanvas>
         </Container>
       </Navbar>
