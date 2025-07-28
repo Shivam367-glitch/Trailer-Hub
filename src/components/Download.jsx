8f0c893a08ea2cb0dcc50ffe05ea318b9551d405
Shivam <txt2shivam@gmail.com>
2025-07-28 18:09:30 +0530

    Download Page Improvement

--- src/components/Download.jsx
+++ src/components/Download.jsx
@@ -3,16 +3,23 @@ import { Button, Col, Container, Row } from "react-bootstrap";
 const Download = () => {
   return (
     <Container fluid>
-      <Row className="mt-3" >
-        <Col className="text-center text-white    bg-dark opacity-90 py-5">
-          <p>To get app like experience, download the APK below:</p>
-          <Button variant="danger" href="base.apk" download={"base.apk"} className="mt-4">
+      <Row className="mt-3">
+        <Col className="text-center text-white bg-dark opacity-90 py-5">
+          <p>To get app-like experience, download the APK below:</p>
+          <Button
+            as="a"
+            href="https://drive.google.com/file/d/1ifizn27s4MLcTifapIUMooGjLNFMM7in/view?usp=sharing"
+            target="_blank"
+            rel="noopener noreferrer"
+            variant="danger"
+            className="mt-4"
+          >
             Download
           </Button>
         </Col>
       </Row>
     </Container>
-  )
-}
+  );
+};
 
 export default Download;
