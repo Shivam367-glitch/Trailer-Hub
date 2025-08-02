0a9b84fb766508c2f367a9a278bc5aca1beed936
Shivam <txt2shivam@gmail.com>
2025-08-02 18:41:46 +0530

    Download Page

--- src/components/Download.jsx
+++ src/components/Download.jsx
@@ -1,36 +1,51 @@
+
 import { Button, Col, Container, Row } from "react-bootstrap";
+import { APP_URL } from "../utils/Constants";
+import {QRCodeCanvas} from 'qrcode.react'; 
 
 const Download = () => {
+
   return (
-    <Container fluid className="bg-dark text-white mt-4">
-      <Row className="justify-content-center align-items-center text-center  py-5">
-        <Col md={8}>
-          <h1 className="display-6 fw-bold mb-3">Get Our Movie Trailer App</h1>
-          <p className="lead mb-4">
-            Watch the latest movie trailers — anytime, anywhere. Stay updated with what’s coming next in cinema.
-          </p>
-
-          <Button
-            as="a"
-            href="https://drive.google.com/file/d/1ifizn27s4MLcTifapIUMooGjLNFMM7in/view?usp=sharing"
-            target="_blank"
-            rel="noopener noreferrer"
-            variant="danger"
-            size="lg"
-            className="px-4"
-          >
-            Download Now
-          </Button>
-        </Col>
-      </Row>
-
-      <Row className="justify-content-center mt-5">
+    <Container fluid className="bg-dark text-white mt-4 px-3 px-md-5">
+      <Row className="justify-content-center align-items-center py-5 flex-column flex-md-row gap-3 gap-md-0">
+      
         <Col md={6} className="text-center">
           <img
             src="trailer-preview.png"
             alt="App Preview"
-            className="img-fluid rounded shadow"
+            className="img-fluid"
+            style={{ maxHeight: "500px" }}
           />
+        </Col>
+
+        <Col md={6} className="d-flex flex-column gap-2 text-center text-md-start ">
+          <h1 className="display-6 fw-bold mb-3">Download Our App</h1>
+          <p className=" mb-4">
+             Watch the latest movie trailers — anytime, anywhere. Stay updated with what’s coming next in cinema.
+          </p>
+
+         <div className="d-flex  justify-content-center justify-content-md-start gap-5">
+
+          <QRCodeCanvas
+        value={APP_URL}
+        size={128}
+        bgColor="#ffffff"
+        fgColor="#000000"
+        level="H" 
+        marginSize={4}
+      />
+           <Button
+            as="a"
+            href={APP_URL}
+            target="_blank"
+            rel="noopener noreferrer"
+            variant="danger"
+            size="lg"
+            className="me-3 mb-2 align-self-center "
+          >
+            Download Now
+          </Button>
+         </div>
         </Col>
       </Row>
     </Container>
